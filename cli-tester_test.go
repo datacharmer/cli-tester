4d92d3789c40806c5561862c680e06196a572f55
Giuseppe Maxia <g.maxia@gmail.com>
2021-03-15 05:02:07 +0100

    Add logging to test

--- cli-tester_test.go
+++ cli-tester_test.go
@@ -2,23 +2,45 @@ package cli_tester
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"testing"
 )
 
 func C(args ...string) CommandSet {
 	return NewCommandSet(args...)
 }
+func CS(args string) CommandSet {
+	return NewStringCommandSet(args)
+}
 
+// TODO: add run with an expected failure
 func TestCliTest_Run(t *testing.T) {
-	ct := NewCliTest("dbdeployer", "1.59.0", "--version", true)
+	var logger *log.Logger
+	if os.Getenv("LOG_ON_SCREEN") != "" {
+		logger = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	} else {
+		var logfileName = "test.log"
+		logFile, err := os.OpenFile(logfileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+		if err != nil {
+			t.Errorf("error opening log file %s: %s", logfileName, err)
+			return
+		}
+		defer logFile.Close()
+		logger = log.New(logFile, "", log.Ldate|log.Ltime)
+		defer os.Remove(logfileName)
+	}
+	ct := NewCliTest("dbdeployer", "1.59.0", "--version", true, logger)
 
 	ct.Add(&CliSet{
-		Name:    "deploy single",
-		Command: C("dbdeployer", "deploy", "single", "8.0.23"),
+		Name: "deploy single",
+		//Command: C("dbdeployer", "deploy", "single", "8.0.23"),
+		Command: CS("dbdeployer deploy single 8.0.23"),
 	}, "started")
 	ct.Add(&CliSet{
-		Name:    "delete single",
-		Command: C("dbdeployer", "delete", "msb_8_0_23"),
+		Name: "delete single",
+		//Command: C("dbdeployer", "delete", "msb_8_0_23"),
+		Command: CS("dbdeployer delete msb_8_0_23"),
 	}, "deleted")
 	ct.Add(&CliSet{
 		Name:    "deploy replication",
@@ -35,7 +57,9 @@ func TestCliTest_Run(t *testing.T) {
 	}
 	for i, err := range errors {
 		fmt.Printf("ErrorMessage %d %#v\n", i, err.ErrorMessage)
-		fmt.Printf("StdOut %#v\n", err.RunSet.Command.StdOut)
-		fmt.Printf("ErrOut %#v\n", err.RunSet.Command.ErrOut)
+		if err.RunSet != nil {
+			fmt.Printf("StdOut %#v\n", err.RunSet.Command.StdOut)
+			fmt.Printf("ErrOut %#v\n", err.RunSet.Command.ErrOut)
+		}
 	}
 }
